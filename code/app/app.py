05100a1f7dccf5ea1a459e28c8031f48d363c45c
rdkkz <Bolszewo11>
2020-06-25 21:22:19 +0200

    update

--- code/app/app.py
+++
@@ -1,8 +0,0 @@
-from code.database.program import funkcja_testowa as funkcja
-from code.database.program2 import funkcja_testowa, funkcja_2
-
-funkcja()
-funkcja_testowa()
-
-# import sys
-# print(sys.path)
