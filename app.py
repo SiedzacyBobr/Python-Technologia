2ee7dc178023d6b4bebe43e4358cb655f2fa0dec
rdkkz <Bolszewo11>
2020-06-25 20:30:24 +0200

    update

--- app.py
+++
@@ -1,3 +0,0 @@
-import program
-
-program.funkcja_testowa()
---
+++ code/database/program2.py
@@ -0,0 +1,9 @@
+def funkcja_testowa():
+    print("hello_world")
+
+def funkcja_2():
+    print("test1")
+
+if __name__ == '__main__':
+    print(f"uruchomilismy modul program {__name__}")
+    liczba = 1
